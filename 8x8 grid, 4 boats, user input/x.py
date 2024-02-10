8e46afe37cd1816f362429554e60cacca824777f
matsyl1 <156823556+matsyl1@users.noreply.github.com>
2024-02-10 16:30:33 +0100

    Update x.py

--- 8x8 grid, 4 boats, user input/x.py
+++ 8x8 grid, 4 boats, user input/x.py
@@ -35,7 +35,6 @@ def create_boat_coords(size, used_coords):
 boat_5 = create_boat_coords(5, [])
 for (x, y) in boat_5:
     grid[y, x] = "5"
-
 # update used_coords with boat 5
 used_coords = boat_5[:]
 
@@ -43,9 +42,24 @@ used_coords = boat_5[:]
 boat_4 = create_boat_coords(4, used_coords)
 for (x, y) in boat_4:
     grid[y, x] = "4"
-
 # update used_coords with boat 4
 used_coords.extend(boat_4)
 
+# boat 3
+boat_3 = create_boat_coords(3, used_coords)
+for (x, y) in boat_3:
+    grid[y, x] = "3"
+# update used_coords with boat 3
+used_coords.extend(boat_3)
+
+# boat 2
+boat_2 = create_boat_coords(2, used_coords)
+for (x, y) in boat_2:
+    grid[y, x] = "2"
+# update used_coords with boat 2
+used_coords.extend(boat_2)
+
+
+# print grid and all used coords
 print(grid)
 print(used_coords)
